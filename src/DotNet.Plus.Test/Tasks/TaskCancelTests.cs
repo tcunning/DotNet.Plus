19bd0c6d98f1d04610a6350afb358c9d5fb86ea5
Tod Cunningham <tod.cunningham@gmail.com>
2020-08-19 09:55:43 -0400

    First pass implementation of CancelWhen

---
+++ src/DotNet.Plus.Test/Tasks/TaskCancelTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace DotNet.Plus.Tasks.Tests
+{
+    [TestClass]
+    public class TaskCancelTests
+    {
+        [TestMethod]
+        [Timeout(100)]
+        public async Task CancelWhenWithResultImmediateTestAsync()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            using var cts = new CancellationTokenSource();
+            tcs.SetResult(10);
+
+            var result = await tcs.CancelWhen(cts.Token);
+            result.ShouldBe(10);
+        }
+
+
+        [TestMethod]
+        [Timeout(100)]
+        public void CancelWhenCanceledTestAsync()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            using var cts = new CancellationTokenSource();
+            cts.Cancel();
+            cts.IsCancellationRequested.ShouldBe(true);
+
+            Should.Throw<TaskCanceledException>(async () => await tcs.CancelWhen(cts.Token));
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public void CancelWhenTimeoutTestAsync()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            Should.Throw<TimeoutException>(async () => await tcs.CancelWhen(2));
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public async Task CancelWhenWithResultTestAsync()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            using var cts = new CancellationTokenSource();
+
+            _ = Task.Run(async () => {
+                await Task.Delay(1, CancellationToken.None);
+                tcs.SetResult(10);
+
+            });
+
+            var result = await tcs.CancelWhen(cts.Token);
+            result.ShouldBe(10);
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public void CancelWhenExceptionTestAsync()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            using var cts = new CancellationTokenSource();
+
+            _ = Task.Run(async () => {
+                await Task.Delay(2, CancellationToken.None);
+                tcs.SetException(new ArgumentException());
+            });
+
+            Should.Throw<ArgumentException>(async () => await tcs.CancelWhen(cts.Token));
+        }
+
+        [TestMethod]
+        [Timeout(100)]
+        public void CancelWhenTaskCanceledTestAsync()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            using var cts = new CancellationTokenSource();
+
+            _ = Task.Run(async () => {
+                await Task.Delay(2, CancellationToken.None);
+                tcs.SetCanceled();
+            });
+
+            Should.Throw<TaskCanceledException>(async () => await tcs.CancelWhen(cts.Token));
+        }
+
+
+    }
+}
---
+++ src/DotNet.Plus/Tasks/TaskCancel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNet.Plus.Tasks
+{
+    public static class TaskCancel
+    {
+        public static async Task<TResult> CancelWhen<TResult>(this Task<TResult> task, CancellationToken cancellationToken, int timeoutMs)
+        {
+            using var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            if( timeoutMs > 0 )
+                cancellationTokenSource.CancelAfter(timeoutMs);
+
+            var cancellationTcs = new TaskCompletionSource<TResult>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            await Task.WhenAny(task, cancellationTcs.Task);
+
+            return task.Result;
+        }
+
+        public static Task<TResult> CancelWhen<TResult>(this TaskCompletionSource<TResult> tcs, int timeoutMs) =>
+            CancelWhen(tcs, CancellationToken.None, timeoutMs);
+        
+        public static async Task<TResult> CancelWhen<TResult>(this TaskCompletionSource<TResult> tcs, CancellationToken cancellationToken, int timeoutMs = Timeout.Infinite)
+        {
+            using var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            if( timeoutMs > 0 )
+                cancellationTokenSource.CancelAfter(timeoutMs);
+
+            TaskCompletionSource<TResult> cancellationTcs = new TaskCompletionSource<TResult>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            void UpdateWhenCanceledOrTimedOut() {
+                if( cancellationToken.IsCancellationRequested )
+                    cancellationTcs.SetCanceled();
+                else // We must have timed out
+                    cancellationTcs.SetException(new TimeoutException($"Timed out after {timeoutMs}ms"));
+            }
+
+            using( cancellationTokenSource.Token.Register(UpdateWhenCanceledOrTimedOut) )
+            {
+                // WhenAny will not throw, unless its arguments are invalid.
+                //
+                await Task.WhenAny(tcs.Task, cancellationTcs.Task);
+
+                // If the given tcs was completed then go ahead and use its results.  It will throw if the tcs was
+                // in an exception or canceled state.
+                //
+                if( tcs.Task.IsCompleted )
+                    return tcs.Task.Result;  // This will throw the tcs.Task.Exception or Canceled Exception if needed
+
+                // If our cancellation tcs is in an exception state use it.  It will be in an exception state if the
+                // operation timed out.
+                //
+                if( cancellationTcs.Task.Exception != null ) {
+                    tcs.TrySetException(cancellationTcs.Task.Exception);
+                    throw cancellationTcs.Task.Exception;
+                }
+
+                // If we got here, the only other choice is the operation was canceled
+                //
+                tcs.TrySetCanceled();
+                throw new TaskCanceledException();
+            }
+        }
+
+    }
+}
