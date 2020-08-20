1d470c38d3826fc2ee99b143706e7145e1d1fe99
Tod Cunningham <tod.cunningham@gmail.com>
2020-08-20 01:36:28 -0400

    Added WhereNot

---
+++ src/DotNet.Plus/Core/Linq.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DotNet.Plus.Core
+{
+    public static class Linq
+    {
+        public static IEnumerable<TSource> WhereNot<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) =>
+            source.Where((src) => !predicate(src));
+    }
+}
