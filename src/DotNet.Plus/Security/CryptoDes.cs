e1ab528c1b9fe941287b49cbc5e5b97482c7cc5a
Tod Cunningham <tcunning@toast.net>
2020-12-08 16:32:28 -0500

    Initial pass at simple Crypto helpers

---
+++ src/DotNet.Plus/Security/CryptoDes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DotNet.Plus.Security
+{
+    public static partial class Crypto
+    {
+        /// <summary>
+        /// Legacy support for older style DES (Data Encryption Standard) support for simple encryption/decryption. 
+        /// </summary>
+        /// <param name="encryptedBase64String">The  base 64 encrypted string to decrypt</param>
+        /// <param name="encryptionKey">The DES key, should be exactly 8 bytes</param>
+        /// <returns>The decrypted result as a string</returns>
+        /// <exception cref="CryptographicException">If there was an error during decryption</exception>
+        public static string DesDecrypt(this string encryptedBase64String, string encryptionKey)
+        {
+            if( string.IsNullOrEmpty(encryptedBase64String) )
+                return "";
+
+            byte[] keyBytes = Encoding.ASCII.GetBytes(encryptionKey);
+            byte[] stringBytes = Convert.FromBase64String(encryptedBase64String);
+
+            using DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            using var decryptor = cryptoProvider.CreateDecryptor(keyBytes, keyBytes);
+
+            using MemoryStream memoryStream = new MemoryStream(stringBytes);
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);  // Will be disposed by StreamReader
+            using StreamReader reader = new StreamReader(cryptoStream);
+
+            return reader.ReadToEnd();
+        }
+
+        /// <summary>
+        /// Legacy support for older style DES (Data Encryption Standard) support for simple encryption/decryption. 
+        /// </summary>
+        /// <param name="originalString">The unencrypted string to encrypt</param>
+        /// <param name="encryptionKey">The DES key, should be exactly 8 bytes</param>
+        /// <returns>The encrypted result as a base 64 string</returns>
+        /// <exception cref="CryptographicException">If there was an error during encryption</exception>
+        public static string DesEncrypt(this string originalString, string encryptionKey)
+        {
+            if( string.IsNullOrEmpty(originalString) )
+                return "";
+
+            byte[] keyBytes = Encoding.ASCII.GetBytes(encryptionKey);
+            byte[] stringBytes = Encoding.ASCII.GetBytes(originalString);
+
+            using DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            using var encryptor = cryptoProvider.CreateEncryptor(keyBytes, keyBytes);
+
+            using MemoryStream input = new MemoryStream(stringBytes);
+            CryptoStream cryptoStream = new CryptoStream(input, encryptor, CryptoStreamMode.Read);       // Will be disposed by StreamReader
+            using MemoryStream output = new MemoryStream();
+            cryptoStream.CopyTo(output);
+            var encryptedBytes = output.ToArray();
+
+            return Convert.ToBase64String(encryptedBytes);
+        }
+    }
+}
---
+++ src/DotNet.Plus/Security/CryptoMd5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DotNet.Plus.Security
+{
+    public static partial class Crypto
+    {
+        /// <summary>
+        /// Generate an MD5 Hash from the given string.  The security of the MD5 hash function is severely compromised, but
+        /// is included for compatibility with system that still require it.
+        /// </summary>
+        /// <param name="fromString"></param>
+        /// <returns></returns>
+        public static byte[] Md5Hash(this string fromString)
+        {
+            using var md5Crypto = new MD5CryptoServiceProvider();
+
+            md5Crypto.Initialize();
+            var hashBytes = md5Crypto.ComputeHash(Encoding.UTF8.GetBytes(fromString));
+            md5Crypto.Clear();
+            return hashBytes ?? throw new ArgumentException($"Unable to compute hash for {fromString}", nameof(fromString));
+        }
+    }
+}
